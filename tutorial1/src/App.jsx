5592a1a600dbc78dfe5e39030f7d4b027d2d14db
Junior Teudjio <junior@Juniors-MacBook-Pro.local>
2022-04-12 13:36:18 +0100

    tutorial1: Installed tailwind successfully

---
+++ tutorial1/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
+      It works.
+    </span>
+  );
+}
+
+export default App;
